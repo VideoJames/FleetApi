b4e31aae849a62519f15e65af32b9a625cff51a3
Jame <lambourn.james@gmail.com>
2026-04-06 18:31:38 +1200

    Added new DeviceReadings dbcontext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,5 +11,37 @@ namespace WorkflowApi.Data
         }
 
         public DbSet<User> Users => Set<User>();
+        public DbSet<DeviceReading> DeviceReadings => Set<DeviceReading>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DeviceReading>(entity =>
+            {
+                // Precision
+                entity.Property(e => e.BatteryVoltage)
+                    .HasPrecision(5, 2);
+
+                entity.Property(e => e.Temperature)
+                    .HasPrecision(5, 2);
+
+                // Relationship
+                entity.HasOne(e => e.User)
+                    .WithMany(u => u.DeviceReadings)
+                    .HasForeignKey(e => e.UserId)
+                    .OnDelete(DeleteBehavior.SetNull);
+
+                // Indexes
+                entity.HasIndex(e => e.UserId)
+                    .HasDatabaseName("IX_DeviceReadings_UserId");
+
+                entity.HasIndex(e => e.DeviceName)
+                    .HasDatabaseName("IX_DeviceReadings_DeviceName");
+
+                entity.HasIndex(e => e.RecordedAt)
+                    .HasDatabaseName("IX_DeviceReadings_RecordedAt");
+            });
+        }
     }
 }
